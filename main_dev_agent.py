10fce02888f3ae30b5f740191731a17f1dba53aa
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-02 11:02:31 -0600

    Delete main_dev_agent.py

--- main_dev_agent.py
+++
@@ -1,5 +0,0 @@
-{"def search(query):
-  # RetreiveDocumentBasedOnRequest will be called here to get the relevant data
-  return "Search results for %s" % query
-def CreateOrEditFile(message, filename, code):
-  print("Creating or editing file: %s") % message
