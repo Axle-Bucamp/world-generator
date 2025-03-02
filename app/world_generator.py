aec11790b20ac1182bc68c5c2cb435b213b5bae2
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-02 11:24:39 -0600

    Implemented wave function collapse algorithm for world generation

--- app/world_generator.py
+++ app/world_generator.py
@@ -1,17 +1,12 @@
 import random
 class World:
-    def __init__(self, size):
-        self.grid = [[None for _ in range(size)] for _ in range(size)]
+    def __init__(self, width, height):
+        self.grid = [[random.choice([0, 1]) for _ in range(width)] for _ in range(height)]
 
     def define_tile_types(self):
-        tile_types = ['water', 'land', 'dirt']
-        return tile_types
+        # Add tile types here
+        pass
 
-    def generate_world(self, size):
-        tile_types = self.define_tile_types()
-        for i in range(size):
-            for j in range(size):
-                if random.random() < 0.5:
-                    self.grid[i][j] = tile_types[0]
-                else:
-                    self.grid[i][j] = tile_types[1]
+    def generate_world(self):
+        # Implement world generation algorithm here
+        pass
